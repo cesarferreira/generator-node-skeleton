9733883ad382d335627aac0243538dc6f44ef224
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2017-07-15 02:17:27 +0100

    added update notifier

--- generators/app/index.js
+++ generators/app/index.js
@@ -47,7 +47,7 @@ module.exports = Generator.extend({
     const devPackages = ['eslint', 'xo'];
     this.yarnInstall(devPackages, {dev: true});
 
-    const packages = ['node-time-log'];
+    const packages = ['node-time-log', 'update-notifier'];
     this.yarnInstall(packages, {dev: false});
   }
 });
--- generators/app/templates/index.js
+++ generators/app/templates/index.js
@@ -1,3 +1,10 @@
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
 const log = require('node-time-log').SimpleLog;
 
+// This will notify the user to update the
+// package if a new version is released
+updateNotifier({pkg}).notify();
+
+// main code //
 log(`Hello world!`);
