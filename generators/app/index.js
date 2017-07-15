57d214dcc94d9aee5e2d7a1ff89290011996e62e
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2017-07-15 02:22:46 +0100

    added chalk

--- generators/app/index.js
+++ generators/app/index.js
@@ -47,7 +47,7 @@ module.exports = Generator.extend({
     const devPackages = ['eslint', 'xo'];
     this.yarnInstall(devPackages, {dev: true});
 
-    const packages = ['node-time-log', 'update-notifier'];
+    const packages = ['node-time-log', 'update-notifier', 'chalk'];
     this.yarnInstall(packages, {dev: false});
   }
 });
